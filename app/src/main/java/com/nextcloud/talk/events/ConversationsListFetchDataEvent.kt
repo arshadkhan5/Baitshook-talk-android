26af445eae01bc2b3ee94de4fa1d25e9e9d4c4ff
macbook <engrarshadkhan505@gmail.com>
2026-02-24 17:05:31 +0300

    refactored

--- app/src/main/java/com/nextcloud/talk/events/ConversationsListFetchDataEvent.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Nextcloud Talk - Android Client
- *
- * SPDX-FileCopyrightText: 2022 Marcel Hibbe <dev@mhibbe.de>
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-package com.nextcloud.talk.events
-
-class ConversationsListFetchDataEvent
--- app/src/main/java/com/nextcloud/talk/utils/NoSupportedApiException.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Nextcloud Talk - Android Client
- *
- * SPDX-FileCopyrightText: 2021 Joas Schilling <coding@schilljs.com>
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
-package com.nextcloud.talk.utils
-
-class NoSupportedApiException : RuntimeException("No supported API version found")
